2a1ad1ae3c2fe2c1c96554112051296dd1f0e9bb
Nixon1018 <nixon.joshwa@tringapps.com>
2023-02-10 16:13:05 +0530

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,35 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.HashMap;
+import java.util.logging.Logger;
+
+public class Main{
+    public static final Logger Log = Logger.getLogger("InfoLogging");
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        HashMap<String,String> hmap = new HashMap<String,String>();
+
+        hmap.put("std1","luke");
+        hmap.put("std2","john");
+        hmap.put("std3","rick");
+        hmap.put("std4","brook");
+        hmap.put("std5","patrick");
+        String hash = ""+hmap;
+        Log.info(hash);
+        String size = "Size: "+hmap.size();
+        Log.info(size);
+        String key = ""+hmap.keySet();
+        Log.info(key);
+        String val = ""+hmap.values();
+        Log.info(val);
+        hmap.remove("std3");
+        hash = ""+hmap;
+        Log.info(hash);
+        hmap.replace("std2", "pravin");
+        hash = ""+hmap;
+        Log.info(hash);
+        Log.info(hmap.get("std4"));
+
     }
+
+
 }
