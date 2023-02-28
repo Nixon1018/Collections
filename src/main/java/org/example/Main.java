45f2dcc59a23f917570d10fb01fd44d15ba5d99d
Nixon1018 <123439949+Nixon1018@users.noreply.github.com>
2023-02-28 09:50:23 +0530

    Add files via upload

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,35 @@
+package org.example;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String [] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1.Array 2.Arraylist 3.list 4.Quiet");
+        int n=1;
+        do {
+            int choice = sc.nextInt();
+            if(choice==1)
+            {
+                Array a = new Array();
+                a.arr();
+            }
+            if(choice==2)
+            {
+                Arraylist al = new Arraylist();
+                al.arraylist();
+            }
+            if (choice==3)
+            {
+                Linkedlist ll = new Linkedlist();
+                ll.linkedlist();
+            }
+            if(choice==4)
+            {
+                n=0;
+            }
+        }while(n==1);
+
+    }
+}
---
+++ src/main/java/org/example/Node.java
@@ -0,0 +1,11 @@
+package org.example;
+public class Node
+{
+    int data;
+    Node nextnode;
+    Node(int data)
+    {
+        this.data=data;
+    }
+
+}
