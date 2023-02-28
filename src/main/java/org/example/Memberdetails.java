0630eb4e4349d0bd5d06950efa01627c38b41988
Nixon1018 <123439949+Nixon1018@users.noreply.github.com>
2023-02-28 11:12:07 +0530

    Update Memberdetails.java

--- src/main/java/org/example/Memberdetails.java
+++ src/main/java/org/example/Memberdetails.java
@@ -6,6 +6,7 @@ public class Memberdetails {
     int id;
 
     Memberdetails(String membername, String memberaddress, int id) {
+        this.membername=membername;
         this.memberaddress = memberaddress;
         this.id = id;
     }
